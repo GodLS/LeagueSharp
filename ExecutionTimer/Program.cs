8664258afa097a59e9e7ba9ed7078726283d2079
GodLS <lolskinmethod@gmail.com>
2016-11-30 13:06:33 -0500

    Execution Timer

---
+++ ExecutionTimer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace ExecutionTimer
+{
+    internal class Program
+    {
+        internal static Stopwatch stop_watch = new Stopwatch();
+        internal static Obj_AI_Base last_hit_player;
+        internal static string text = "executed";
+        internal static Menu menu = new Menu("Execution Timer", "Execution Timer", true);
+
+        private static void Main(string[] args) 
+        {
+            CustomEvents.Game.OnGameLoad += eventArgs =>
+            {
+                AttackableUnit.OnDamage += OnDamage;
+                Drawing.OnDraw += OnDraw;
+                Game.PrintChat("[Execution<font color='#79BAEC'>Timer</font>]: <font color='#FFFFFF'>" + "Loaded!</font>");
+
+                menu.AddItem(new MenuItem("enabled", "Enabled", false).SetValue(true));
+                menu.AddItem(new MenuItem("xpos", "X Position").SetValue(new Slider(100, Drawing.Width)));
+                menu.AddItem(new MenuItem("ypos", "Y Position").SetValue(new Slider(100, Drawing.Height)));
+
+                menu.AddToMainMenu();
+            };
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            if (!menu.Item("enabled").IsActive())
+                return;
+
+            if (stop_watch.ElapsedMilliseconds > 14000)       
+                text = "Executed";
+   
+            else if (last_hit_player != null)
+                text = last_hit_player.CharData.BaseSkinName + " gets the kill for " + (14 - stop_watch.Elapsed.Seconds) + " more second(s)";
+
+            Drawing.DrawText(menu.Item("xpos").GetValue<Slider>().Value, menu.Item("ypos").GetValue<Slider>().Value, System.Drawing.Color.White, text);
+        }
+
+        private static void OnDamage(AttackableUnit sender, AttackableUnitDamageEventArgs args)
+        {
+            if (!menu.Item("enabled").IsActive())
+                return;
+
+            if (!sender.IsMe)
+                return;
+
+            foreach (var e in HeroManager.Enemies.Where(e => e.IsValid && e.NetworkId == args.SourceNetworkId))
+            {
+                stop_watch.Restart();
+                last_hit_player = e;
+            }
+        }
+    }
+}
---
+++ ExecutionTimer/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExecutionTimer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExecutionTimer")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34b7fd04-5969-41e1-901a-627b6a98e2a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
